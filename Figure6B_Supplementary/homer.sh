0a165d701fd9cf2721c1594e96a28920d3e5caa8
sinnamone <simone.puccio@humanitasresearch.it>
2026-09-10 11:03:55 +0200

    update

---
+++ Figure6B_Supplementary/homer.sh
@@ -0,0 +1,10 @@
+
+findMotifs.pl /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/InputFasta/CD57_unique.fasta fasta /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/Output/CD57/ -fastaBg /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/InputFasta/PD1_unique.fasta  -p 20  -mis 1 -len 6,7,8,9,10,11,12 -mset vertebrates 
+
+findMotifs.pl /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/InputFasta/CD57_unique_promoter.fasta fasta /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/Output/CD57_promoter/ -fastaBg /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/InputFasta/PD1_unique_promoter.fasta -p 20 -mis 1 -len 6,7,8,9,10,11,12 -mset vertebrates 
+
+findMotifs.pl /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/InputFasta/PD1_unique.fasta fasta /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/Output/PD1/ -fastaBg /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/InputFasta/CD57_unique.fasta  -p 20 -mis 1 -len 6,7,8,9,10,11,12 -mset vertebrates 
+
+findMotifs.pl /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/InputFasta/PD1_unique_promoter.fasta fasta /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/Output/PD1_promoter/ -fastaBg /home/lugli/spuccio/Projects/ATAC_ines_sef/bwa/HOMER/InputFasta/CD57_unique_promoter.fasta  -p 20 -mis 1 -len 6,7,8,9,10,11,12 -mset vertebrates 
+
+
